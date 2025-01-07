beacf4f32c696cb10d98d05b8f6b9ca20a1160e7
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-01-07 17:06:57 -0600

    Making better structure with services

--- routes/movies.js
+++ routes/movies.js
@@ -1,20 +1,36 @@
 
 const express = require('express')
+
+/** Lo quitamos, ya que ahora se convirtio en un servicio "MoviesService" **/
 // importamos el archivo don de guardamos nuestras peliculas generadas
-const moviesMocks = require('./utils/mocks/moviesMocks')
+/*const moviesMocks = require('../utils/mocks/moviesMocks')*/
+// Importamos la capa de servicios que creamos para el controlador
+const MoviesServices = require('../services/moviesServices')
 
 
 function moviesAPI (app){
+    
+    // Preparamos la clase de servicio que creamos para usarla en el controlador
+    const moviesServices = new MoviesServices()
 
-    // READ
+    // Definiendo la ruta en la que trabajara el router
     const router = express.Router()
     app.use('/api/movies',router)
+    
 
+    // READ
     router.get('/', async (req, res, next) => {
         try{
+            // Etiquetas para filtrar peliculas (Datos en la query (url))
+            const tags = req.query
+
             // Aqui va la consulta a la base de datos
             /*Como no hay aun una base de datos, se accedera a los datos que guardamos en moviesMocks*/
-            const moviesList = await Promise.resolve(moviesMocks)
+            /** Lo quitamos, ya que ahora se convirtio en un servicio "MoviesService" **/
+            /*const moviesList = await Promise.resolve(moviesMocks)*/
+
+            // Ahora usamos el metodo de nuestra clase de nuestro servicio, Se le manda un objeto "{}" para filtrar peliculas
+            const moviesList = await moviesServices.getMoviesService({tags})
 
             // Esto significa que hubo exito en la consulta y devolvemos en json la respuesta
             res.status(200).json({
@@ -26,17 +42,41 @@ function moviesAPI (app){
         }
     })
 
+    // READ one movie
+    router.get('/:movieId', async (req, res, next) => {
+        
+        try{
+            // Obteniendo los parametros para bucar pelicula por id
+            const {movieId} = req.params
+
+            /*const movie = await Promise.resolve(moviesMocks.movies[0])*/
+            const movie = await moviesServices.getOneMovieService({movieId})
+
+            // Esto significa que hubo exito en la consulta y devolvemos en json la respuesta
+            res.status(200).json({
+                data:movie,
+                message:'movie by id'
+            })
+        }catch(err){
+            next(err)
+        }
+    })
+
 
     // CREATE
     router.post('/', async (req, res, next) => {
 
         try{
-            const createdMovie = await Promise.resolve(moviesMocks.movies[0].id)
-            
+            // Obtenemos los datos del body, que es donde mandaremos los datos de la pelicula que crearemos
+            const {body:movie} = req
+            /*const createdMovie = await Promise.resolve(moviesMocks.movies[0].id)*/
+            // Mandamos llamar el metodo para crear una pelicula mandando como parametro los datos de la pelicula
+            const createdMovie = await moviesServices.createMovieService({movie})
+
             // Esto significa que se creo exitosamente
             res.status(201).json({
                 data:createdMovie, 
-                message:'created movie'
+                message:'movie created'
             })
         }catch(err){
             next(err)
@@ -48,12 +88,18 @@ function moviesAPI (app){
     router.put('/:movieId', async (req, res, next) => {
 
         try{
-            const updatedMovie = await Promise.resolve(moviesMocks.movies[0].id)
-            
+            // Obteniendo los parametros para bucar pelicula por id
+            const {movieId} = req.params
+            // Obtenemos los datos del body, que es donde mandaremos los datos de la pelicula que crearemos
+            const {body:movie} = req
+            /*const updatedMovie = await Promise.resolve(moviesMocks.movies[0].id)*/
+            // Llamamos al metodo para actualizar una pelicula mandandole el id para encontrar la pelicula y los datos nuevos
+            const updatedMovie = await moviesServices.updateMovieService({movieId, movie})
+
             // Esto significa que se actualizo exitosamente
             res.status(200).json({
                 data:updatedMovie,
-                message:'updated movie'
+                message:'movie updated'
             })
         }catch(err){
             next(err)
@@ -65,12 +111,15 @@ function moviesAPI (app){
     router.delete('/:movieId', async (req, res, next) => {
 
         try{
-            const deletedMovie = await Promise.resolve(moviesMocks.movies[0].id)
-            
+            // Obteniendo los parametros para bucar pelicula por id
+            const {movieId} = req.params
+            /*const deletedMovie = await Promise.resolve(moviesMocks.movies[0].id)*/
+            const deletedMovie = await moviesServices.deleteMovieService({movieId})
+
             // Esto significa que se elimino exitosamente
             res.status(200).json({
                 data:deletedMovie, 
-                message:'deleted movie'
+                message:'movie deleted'
             })
         }catch(err){
             next(err)
---
+++ services/moviesServices.js
@@ -0,0 +1,61 @@
+
+// Importamos el archivo donde guardamos nuestras peliculas generadas
+const moviesMocks = require('../utils/mocks/moviesMocks')
+
+
+class MoviesServices{
+
+    // Metodo para obtener una lista de peliculas, ya sea completa o muchas filtradas
+        /* Parameters: 
+            -tags -> Etiquetas para filtrar peliculas
+         */
+        /* Response:  Los datos de todas las peliculas registradas o una lista vacia */
+    async getMoviesService(){
+        const moviesList = await Promise.resolve(moviesMocks.movies)
+        return moviesList || []
+    }
+
+    // Metodo para obtener una sola pelicula filtrada por id
+        /* Parameters: 
+            -movieId -> El id de una pelicula para buscar sus datos
+         */
+        /* Response:  Los datos de la pelicula encontrada o un objeto vacio */
+    async getOneMovieService(movieId){
+        const movie = await Promise.resolve(moviesMocks.movies[0])
+        return movie || {}
+    }
+
+    // Se crea una nueva pelicula
+        /* Parameters: 
+            -
+        */
+       /* Response: Los datos de la pelicula creada*/
+    async createMovieService(){
+        const movie = await Promise.resolve(moviesMocks.movies[0])
+        return movie
+    }
+    
+    // Se actualiza una pelicula
+        /* Parameters: 
+            -movieId -> El id de una pelicula para actualizar sus datos
+         */
+        /* Response:  Los datos de la pelicula actualizada */
+    async updateMovieService(movieId){ 
+        const updatedMovie = await Promise.resolve(moviesMocks.movies[0])
+        return updatedMovie
+    }
+    
+    // Se elimina una pelicula
+        /* Parameters: 
+            -movieId -> El id de una pelicula para eliminarla de la base de datos
+         */
+        /* Response:  El id de la pelicula eliminada */
+    async deleteMovieService(movieId){
+        const deletedMovie = await Promise.resolve(moviesMocks.movies[0])
+        return deletedMovie.id
+    }
+
+}
+
+
+module.exports = MoviesServices
