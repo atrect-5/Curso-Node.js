f1ec123f1706d4c34836c89883fd01c95b619d86
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-01-07 18:05:04 -0600

    Cleaning workspace and adding comments

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -12,12 +12,7 @@ const getMovies = async (req, res, next) => {
         // Etiquetas para filtrar peliculas (Datos en la query (url))
         const tags = req.query
 
-        // Aqui va la consulta a la base de datos
-        /*Como no hay aun una base de datos, se accedera a los datos que guardamos en moviesMocks*/
-        /** Lo quitamos, ya que ahora se convirtio en un servicio "MoviesService" **/
-        /*const moviesList = await Promise.resolve(moviesMocks)*/
-
-        // Ahora usamos el metodo de nuestra clase de nuestro servicio, Se le manda un objeto "{}" para filtrar peliculas
+        // Usamos el metodo de nuestra clase MoviesServices de nuestro servicio, Se le manda un objeto "{}" para filtrar peliculas
         const moviesList = await moviesServices.getMoviesService({tags})
 
         // Esto significa que hubo exito en la consulta y devolvemos en json la respuesta
@@ -32,12 +27,10 @@ const getMovies = async (req, res, next) => {
 
 // Funcion del controlador que usa el servicio para obtiener los datos de una sola pelicula usando el id
 const getOneMovie = async (req, res, next) => {
-        
+    // Obteniendo los parametros para bucar pelicula por id
+    const {movieId} = req.params
     try{
-        // Obteniendo los parametros para bucar pelicula por id
-        const {movieId} = req.params
-
-        /*const movie = await Promise.resolve(moviesMocks.movies[0])*/
+        // Se llama al metodo del servicio para obtener una pelicula segun su id
         const movie = await moviesServices.getOneMovieService({movieId})
 
         // Esto significa que hubo exito en la consulta y devolvemos en json la respuesta
@@ -52,11 +45,9 @@ const getOneMovie = async (req, res, next) => {
 
 // Funcion del controlador que usa el servicio para crear una nueva pelicula
 const createMovie = async (req, res, next) => {
-
+    // Obtenemos los datos del body, que es donde mandaremos los datos de la pelicula que crearemos
+    const {body:movie} = req
     try{
-        // Obtenemos los datos del body, que es donde mandaremos los datos de la pelicula que crearemos
-        const {body:movie} = req
-        /*const createdMovie = await Promise.resolve(moviesMocks.movies[0].id)*/
         // Mandamos llamar el metodo para crear una pelicula mandando como parametro los datos de la pelicula
         const createdMovie = await moviesServices.createMovieService({movie})
 
@@ -72,13 +63,11 @@ const createMovie = async (req, res, next) => {
 
 // Funcion del controlador que usa el servicio para actualizar los datos de una pelicula ya existente
 const updateMovie = async (req, res, next) => {
-
+    // Obteniendo los parametros para bucar pelicula por id
+    const {movieId} = req.params
+    // Obtenemos los datos del body, que es donde mandaremos los datos de la pelicula que actualizaremos
+    const {body:movie} = req
     try{
-        // Obteniendo los parametros para bucar pelicula por id
-        const {movieId} = req.params
-        // Obtenemos los datos del body, que es donde mandaremos los datos de la pelicula que crearemos
-        const {body:movie} = req
-        /*const updatedMovie = await Promise.resolve(moviesMocks.movies[0].id)*/
         // Llamamos al metodo para actualizar una pelicula mandandole el id para encontrar la pelicula y los datos nuevos
         const updatedMovie = await moviesServices.updateMovieService({movieId, movie})
 
@@ -94,11 +83,10 @@ const updateMovie = async (req, res, next) => {
 
 // Funcion del controlador que usa el servicio para eliminar una pelicula de la base de datos segun su id
 const deleteMovie = async (req, res, next) => {
-
+    // Obteniendo los parametros para bucar pelicula por id
+    const {movieId} = req.params
     try{
-        // Obteniendo los parametros para bucar pelicula por id
-        const {movieId} = req.params
-        /*const deletedMovie = await Promise.resolve(moviesMocks.movies[0].id)*/
+        // Utilizamos la funcion para eliminar una pelicula segun su id de nuestro servicio
         const deletedMovie = await moviesServices.deleteMovieService({movieId})
 
         // Esto significa que se elimino exitosamente
--- routes/movies.js
+++ routes/movies.js
@@ -1,46 +1,27 @@
 
 const express = require('express')
 
-/** Lo quitamos, ya que ahora se convirtio en un servicio "MoviesService" **/
-// importamos el archivo don de guardamos nuestras peliculas generadas
-/*const moviesMocks = require('../utils/mocks/moviesMocks')*/
-/** Lo quitamos porque ahora se utiliza el servicio en el controlador **/
-// Importamos la capa de servicios que creamos para el controlador
-/*const MoviesServices = require('../services/moviesServices')*/
-
 // Importamos la lista de los controladores que creamos
 const { getMovies, getOneMovie, createMovie, updateMovie, deleteMovie } = require('../controllers/moviesController')
 
-function moviesAPI (app){
-    
-    /** Ahora se instancia en el controlador **/
-    // Preparamos la clase de servicio que creamos para usarla en el controlador
-    /*const moviesServices = new MoviesServices()*/
+function moviesAPI (app){    
 
     // Definiendo la ruta en la que trabajara el router
     const router = express.Router()
     app.use('/api/movies',router)
 
-
     // READ
     router.get('/', getMovies)
-
-    // READ one movie
     router.get('/:movieId', getOneMovie)
-
 
     // CREATE
     router.post('/', createMovie)
 
-
     // UPDATE 
     router.put('/:movieId', updateMovie)
 
-
     // DELETE
     router.delete('/:movieId', deleteMovie)
-
-
 }
 
 module.exports = moviesAPI
