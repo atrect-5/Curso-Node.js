2baa93b74d26cae1e04d2630827236496923e5a7
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-01-14 19:30:12 -0600

    Successfuly conected to database with mongoose

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const port = process.env.PORT
 // Importamos la funcion que creamos para ver las peliculas guardadas
 const moviesAPI = require('./routes/movies')
 
+/******** CONEXION A LA BASE DE DATOS **********/
+// Obtenemos la url de nuestra base de datos 
+const DBURL = process.env.DBURL
+
+// Libreria que se encargara de la conexion a la base de datos 
+const mongoose = require('mongoose')
+
+// Conectando a la base de datos
+mongoose.connect(DBURL);
+
+// Prueba de agregar un dato a la base de datos
+const collectionName = 'Cats' 
+const Cat = mongoose.model(collectionName, { name: String });
+
+const kitty = new Cat({ name: 'candy' });
+kitty.save().then(() => console.log('Successfully saved'));
+
+
 
 // Rutas de la api
 app.get('/', (req, res) => res.send('Welcome to the app c:'))
